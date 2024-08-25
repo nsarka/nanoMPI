7c8ff36c70335d7610b0362f0459f22eb10ce1af
Nick Sarkauskas <nsarka00@gmail.com>
2024-08-25 17:10:53 -0400

    Start on tuner

--- colls/tuner.h
+++ colls/tuner.h
@@ -1,6 +1,17 @@
 #ifndef NANOMPI_TUNER_H
 #define NANOMPI_TUNER_H
 
+#include "mpi.h"
+#include "allgather/allgather.h"
+#include "allgatherv/allgatherv.h"
+#include "allreduce/allreduce.h"
+#include "alltoall/alltoall.h"
+#include "bcast/bcast.h"
+#include "gather/gather.h"
+#include "reduce/reduce.h"
+#include "reduce_scatter/reduce_scatter.h"
+#include "scatter/scatter.h"
+
 
 
 #endif
