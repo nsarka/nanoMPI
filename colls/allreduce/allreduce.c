e258e5848e60090db68bd2b27e3260ee242528eb
Nick Sarkauskas <nsarka00@gmail.com>
2024-09-22 23:49:23 -0400

    Ring allreduce implementation working, todo handle remainders

--- colls/allreduce/allreduce.c
+++ colls/allreduce/allreduce.c
@@ -19,7 +19,7 @@
 // Ring Allreduce
 // First, do a reduce scatter in a ring fashion. Then, do a ring allgather.
 // Requires the reduce op to be associative and commutative.
-// T_ring = 2(p-1)*α + ()*β + = TODO
+// T_ring = 2(p − 1)α + 2nβ + nγ − (1/p)(2nβ+nγ)
 int MPI_Allreduce_ring(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype,
                        MPI_Op op, MPI_Comm comm)
 {
@@ -28,26 +28,45 @@ int MPI_Allreduce_ring(const void *sendbuf, void *recvbuf, int count, MPI_Dataty
     MPI_Comm_size(comm, &size);
 
     int type_size = nanompi_get_dtype_size(datatype);
-    int send_to = (rank + 1) % size;
-    int recv_from = (rank - 1 + size) % size;
+    int send_to = mod(rank + 1, size);
+    int recv_from = mod(rank - 1, size);
     int chunk_count = count / size;
-    int chunk_rem = count % size;
+    int chunk_size = chunk_count * type_size;
+    int chunk_rem = count % size; // todo: handle remainder instead of assert
+    assert(chunk_rem == 0);
 
     // Copy sendbuf to recvbuf
     memcpy(recvbuf, sendbuf, count * type_size);
 
     void *temp_buf = malloc(count * type_size);
     void *send_buf = malloc(count * type_size);
+    memcpy(send_buf, recvbuf, count * type_size);
 
+    // Reduce-scatter
     for (int i = 0; i < size - 1; i++) {
-        MPI_Send(send_buf, count, datatype, send_to, 0, comm);
-        MPI_Recv(temp_buf, count, datatype, recv_from, 0, comm, MPI_STATUS_IGNORE);
+        ptrdiff_t send_offset = chunk_size * mod(rank - i, size);
+        ptrdiff_t recv_offset = chunk_size * mod(rank - i - 1, size);
+
+        MPI_Send(send_buf + send_offset, chunk_count, datatype, send_to, 0, comm);
+        MPI_Recv(temp_buf + recv_offset, chunk_count, datatype, recv_from, 0, comm, MPI_STATUS_IGNORE);
 
         // Perform reduction operation
-        op.fn(temp_buf, recvbuf, &count, &datatype);
+        op.fn(temp_buf + recv_offset, recvbuf + recv_offset, &chunk_count, &datatype);
 
         // Prepare for next iteration
-        memcpy(send_buf, temp_buf, count * type_size);
+        memcpy(send_buf + recv_offset, recvbuf + recv_offset, chunk_size);
+    }
+
+    // Now, each rank has one completely reduced chunk_size portion of all ranks' sendbuf
+    // stored in chunk index=(rank + i) % size. Copy it to the recvbuf
+
+    // Allgather
+    for (int i = 0; i < size - 1; i++) {
+        ptrdiff_t send_offset = chunk_size * mod(rank - i + 1, size);
+        ptrdiff_t recv_offset = chunk_size * mod(rank - i, size);
+
+        MPI_Send(recvbuf + send_offset, chunk_count, datatype, send_to, 0, comm);
+        MPI_Recv(recvbuf + recv_offset, chunk_count, datatype, recv_from, 0, comm, MPI_STATUS_IGNORE);
     }
 
     free(temp_buf);
